89309bd6873d2f00e94d853eef75d5c4f88b5f51
waveology <123938536+waveology@users.noreply.github.com>
2023-02-17 13:33:20 +0000

    Update lectura_de_datos.py

--- lectura_de_datos.py
+++ lectura_de_datos.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
-def ayuntamiento(ifile, codigo_magnitud=8, codigo_estacion=28079001) :
-
-    # Listado de estaciones
-    # ----------------------
-    estalist = {
+    # Listado de estaciones del Ayuntamiento de Madrid
+    # -------------------------------------------------
+    estalist_ayto = {
     # código       municipio nombre_estación    
     28079001 : ('Pº. Recoletos',                'Baja.- 04/05/2009 (14:00 h.)'),
     28079002 : ('Glta. de Carlos V',            'Baja.- 04/12/2006 (11:00 h.)'),
@@ -52,9 +50,9 @@ def ayuntamiento(ifile, codigo_magnitud=8, codigo_estacion=28079001) :
     28079060 : ('Tres Olivos',                  'Código desde enero 2011'),    
     }
     
-    # Listado de magnitudes
-    # ---------------------
-    maglist = {
+    # Listado de magnitudes del Ayuntamiento de Madrid
+    # -------------------------------------------------
+    maglist_ayto = {
     # código     magnitud         unidades    
     1 	:  ('Dióxido de azufre', 'μg/m³'),
     6 	:  ('Monóxido de carbono', 'mg/m³'),
@@ -74,7 +72,103 @@ def ayuntamiento(ifile, codigo_magnitud=8, codigo_estacion=28079001) :
     43  :  ('Metano', 'mg/m³'),
     44  :  ('Hidrocarburos no metánicos', 'mg/m³')    
     }
+
+    # Listado de magnitudes de la Comunidad de Madrid
+    # -------------------------------------------------
+    maglist_com = {
+    # código     magnitud         unidades    
+    1 	:  ('Dióxido de azufre', 'μg/m³'),
+    6 	:  ('Monóxido de carbono', 'mg/m³'),
+    7 	:  ('Monóxido de nitrógeno', 'μg/m³'),
+    8 	:  ('Dióxido de nitrógeno', 'μg/m³'),
+    9 	:  ('Partículas en suspensión < PM2.5', 'μg/m³'),
+    10 	:  ('Partículas en suspensión < PM10',  'μg/m³'),
+    12 	:  ('Óxidos de nitrógeno', 'μg/m³'),
+    14 	:  ('Ozono', 'μg/m³'),
+    20 	:  ('Tolueno', 'μg/m³'),
+    22 	:  ('Black Carbon', 'μg/m³'),
+    30 	:  ('Benceno', 'μg/m³'),
+    42 	:  ('Hidrocarburos totales', 'mg/m³'),
+    44 	:  ('Hidrocarburos no metánicos', 'mg/m³'),
+    431 :  ('MetaParaXileno', 'μg/m³')
+    }
+        
+    # Listado de estaciones de la Comunidad de Madrid
+    # -------------------------------------------------
+    estalist_com = {
+    # código       municipio nombre_estación    
+    28005002   :    (  5,  'ALCALÁ DE HENARES'), 
+    28006004   :    (  6,  'ALCOBENDAS'), 
+    28007004   :    (  7,  'ALCORCÓN'), 
+    28009001   :	(  9,  'ALGETE'), 
+    28013002   :	( 13,  'ARANJUEZ'), 
+    28014002   :	( 14,  'ARGANDA DEL REY'),
+    28016001   :	( 16,  'EL ATAZAR'),
+    28045002   :	( 45,  'COLMENAR VIEJO'), 
+    28047002   :	( 47,  'COLLADO VILLALB A'), 
+    28049003   :	( 49,  'COSLADA'), 
+    28058004   :	( 58,  'FUENLABRADA'), 
+    28065014   :	( 65,  'GETAFE'), 
+    28067001   :	( 67,  'GUADALIX DE LA SIERRA'), 
+    28074007   :	( 74,  'LEGANÉS'), 
+    28080003   :	( 80,  'MAJADAHONDA'), 
+    28092005   :	( 92,  'MÓSTOLES'), 
+    28102001   :	(102,  'ORUSCO DE TAJUÑA'), 
+    28120001   : 	(120,  'PUERTO DE COTOS'), 
+    28123002   :	(123,  'RIVAS-VACIAMADRID'), 
+    28133002   :	(133,  'SAN MARTÍN DE VALDEIGLESIAS'), 
+    28148004   :	(148,  'TORREJÓN DE ARDOZ'), 
+    28161001   :	(161,  'VALDEMORO'), 
+    28171001   :	(171,  'VILLA DEL PRADO'), 
+    28180001   :	(180,  'VILLAREJO DE SALVANÉS')
+    }
+
+    # Listado de estaciones meteorológicas en Madrid
+    # -----------------------------------------------
+    estalist_meteo = {
+    # código       municipio nombre_estación    
+    28005002   :    (  5,  'ALCALÁ DE HENARES'), 
+    28006004   :    (  6,  'ALCOBENDAS'), 
+    28007004   :    (  7,  'ALCORCÓN'), 
+    28009001   :	(  9,  'ALGETE'), 
+    28013002   :	( 13,  'ARANJUEZ'), 
+    28014002   :	( 14,  'ARGANDA DEL REY'),
+    28016001   :	( 16,  'EL ATAZAR'),
+    28045002   :	( 45,  'COLMENAR VIEJO'), 
+    28047002   :	( 47,  'COLLADO VILLALB A'), 
+    28049003   :	( 49,  'COSLADA'), 
+    28058004   :	( 58,  'FUENLABRADA'), 
+    28065014   :	( 65,  'GETAFE'), 
+    28067001   :	( 67,  'GUADALIX DE LA SIERRA'), 
+    28074007   :	( 74,  'LEGANÉS'), 
+    28080003   :	( 80,  'MAJADAHONDA'), 
+    28092005   :	( 92,  'MÓSTOLES'), 
+    28102001   :	(102,  'ORUSCO DE TAJUÑA'), 
+    28120001   : 	(120,  'PUERTO DE COTOS'), 
+    28123002   :	(123,  'RIVAS-VACIAMADRID'), 
+    28133002   :	(133,  'SAN MARTÍN DE VALDEIGLESIAS'), 
+    28148004   :	(148,  'TORREJÓN DE ARDOZ'), 
+    28161001   :	(161,  'VALDEMORO'), 
+    28171001   :	(171,  'VILLA DEL PRADO'), 
+    28180001   :	(180,  'VILLAREJO DE SALVANÉS')
+    }
     
+    # Listado de magnitudes
+    # ---------------------
+    maglist_meteo = {
+    # código     magnitud                unidades    
+    81 :	('Velocidad del viento',     'm/s'), 
+    82 :	('Dirección del viento',     'º'), 
+    83 :	('Temperatura',              'ºC'), 
+    86 :	('Humedad relativa',         '%'), 
+    87 :	('Presión atmosférica',      'hPa'), 
+    88 :	('Radiación solar',          'W/m2'), 
+    89 :	('Precipitación',            'mm')
+    }
+
+
+
+def ayuntamiento(ifile, codigo_magnitud=8, codigo_estacion=28079001) :
 
     # Lectura de datos
     # -------------------------------------------
@@ -84,7 +178,6 @@ def ayuntamiento(ifile, codigo_magnitud=8, codigo_estacion=28079001) :
     # --------------------------------------
     df = df[ (df['ESTACION'] == int(str(codigo_estacion)[-3:])) 
            & (df['MAGNITUD'] == codigo_magnitud) ]
-
     
     # Eliminamos información innecesaria
     # -----------------------------------------
@@ -103,8 +196,7 @@ def ayuntamiento(ifile, codigo_magnitud=8, codigo_estacion=28079001) :
     # Convertimos el día a valor numérico
     # --------------------------------------------------
     df1['DIA'] = df1['DIA'].apply(lambda x : int(x[1:]))
-    
-    
+        
     # Pasamos los días de columnas a filas con
     # el flag de calidad como valor
     # -----------------------------------------
@@ -146,63 +238,10 @@ def ayuntamiento(ifile, codigo_magnitud=8, codigo_estacion=28079001) :
     # ------------------------------------------
     df.sort_index(inplace=True)
         
-    return df, *maglist[codigo_magnitud], estalist[codigo_estacion][1]
+    return df, *maglist_ayto[codigo_magnitud], estalist_ayto[codigo_estacion][1]
 
 
 def comunidad(ifile, codigo_magnitud=8, codigo_estacion=28067001) :
-
-    # Listado de magnitudes
-    # ---------------------
-    maglist = {
-    # código     magnitud         unidades    
-    1 	:  ('Dióxido de azufre', 'μg/m³'),
-    6 	:  ('Monóxido de carbono', 'mg/m³'),
-    7 	:  ('Monóxido de nitrógeno', 'μg/m³'),
-    8 	:  ('Dióxido de nitrógeno', 'μg/m³'),
-    9 	:  ('Partículas en suspensión < PM2.5', 'μg/m³'),
-    10 	:  ('Partículas en suspensión < PM10',  'μg/m³'),
-    12 	:  ('Óxidos de nitrógeno', 'μg/m³'),
-    14 	:  ('Ozono', 'μg/m³'),
-    20 	:  ('Tolueno', 'μg/m³'),
-    22 	:  ('Black Carbon', 'μg/m³'),
-    30 	:  ('Benceno', 'μg/m³'),
-    42 	:  ('Hidrocarburos totales', 'mg/m³'),
-    44 	:  ('Hidrocarburos no metánicos', 'mg/m³'),
-    431 :  ('MetaParaXileno', 'μg/m³')
-    }
-    
-    
-        
-    # Listado de estaciones
-    # ----------------------
-    estalist = {
-    # código       municipio nombre_estación    
-    28005002   :    (  5,  'ALCALÁ DE HENARES'), 
-    28006004   :    (  6,  'ALCOBENDAS'), 
-    28007004   :    (  7,  'ALCORCÓN'), 
-    28009001   :	(  9,  'ALGETE'), 
-    28013002   :	( 13,  'ARANJUEZ'), 
-    28014002   :	( 14,  'ARGANDA DEL REY'),
-    28016001   :	( 16,  'EL ATAZAR'),
-    28045002   :	( 45,  'COLMENAR VIEJO'), 
-    28047002   :	( 47,  'COLLADO VILLALB A'), 
-    28049003   :	( 49,  'COSLADA'), 
-    28058004   :	( 58,  'FUENLABRADA'), 
-    28065014   :	( 65,  'GETAFE'), 
-    28067001   :	( 67,  'GUADALIX DE LA SIERRA'), 
-    28074007   :	( 74,  'LEGANÉS'), 
-    28080003   :	( 80,  'MAJADAHONDA'), 
-    28092005   :	( 92,  'MÓSTOLES'), 
-    28102001   :	(102,  'ORUSCO DE TAJUÑA'), 
-    28120001   : 	(120,  'PUERTO DE COTOS'), 
-    28123002   :	(123,  'RIVAS-VACIAMADRID'), 
-    28133002   :	(133,  'SAN MARTÍN DE VALDEIGLESIAS'), 
-    28148004   :	(148,  'TORREJÓN DE ARDOZ'), 
-    28161001   :	(161,  'VALDEMORO'), 
-    28171001   :	(171,  'VILLA DEL PRADO'), 
-    28180001   :	(180,  'VILLAREJO DE SALVANÉS')
-    }
-
     
     # Lectura de datos
     # -------------------------------------------
@@ -212,9 +251,8 @@ def comunidad(ifile, codigo_magnitud=8, codigo_estacion=28067001) :
     # --------------------------------------
     df = df[ (df['estacion']  == int(str(codigo_estacion)[-3:])) 
            & (df['magnitud']  == codigo_magnitud) 
-           & (df['municipio'] == estalist[codigo_estacion][0])]
-
-    
+           & (df['municipio'] == estalist_com[codigo_estacion][0])]
+  
     # Tamaño de la muestra
     # -------------------------
     print(' -Info: encontrados datos de %d días' % len(df))
@@ -232,8 +270,7 @@ def comunidad(ifile, codigo_magnitud=8, codigo_estacion=28067001) :
                  var_name='hora',
                  value_name='valor',  
                  )
-    
-    
+       
     # Convertimos la hora a valor numérico
     # --------------------------------------------------
     df1['hora'] = df1['hora'].apply(lambda x : int(x[1:]))
@@ -280,55 +317,11 @@ def comunidad(ifile, codigo_magnitud=8, codigo_estacion=28067001) :
     # ------------------------------------------
     df.sort_index(inplace=True)
     
-    return df, *maglist[codigo_magnitud], estalist[codigo_estacion][1]
+    return df, *maglist_com[codigo_magnitud], estalist_com[codigo_estacion][1]
 
 
 
 def meteo(ifile, codigo_magnitud=83, codigo_estacion=28067001) :
-
-    # Listado de estaciones
-    # ----------------------
-    estalist = {
-    # código       municipio nombre_estación    
-    28005002   :    (  5,  'ALCALÁ DE HENARES'), 
-    28006004   :    (  6,  'ALCOBENDAS'), 
-    28007004   :    (  7,  'ALCORCÓN'), 
-    28009001   :	(  9,  'ALGETE'), 
-    28013002   :	( 13,  'ARANJUEZ'), 
-    28014002   :	( 14,  'ARGANDA DEL REY'),
-    28016001   :	( 16,  'EL ATAZAR'),
-    28045002   :	( 45,  'COLMENAR VIEJO'), 
-    28047002   :	( 47,  'COLLADO VILLALB A'), 
-    28049003   :	( 49,  'COSLADA'), 
-    28058004   :	( 58,  'FUENLABRADA'), 
-    28065014   :	( 65,  'GETAFE'), 
-    28067001   :	( 67,  'GUADALIX DE LA SIERRA'), 
-    28074007   :	( 74,  'LEGANÉS'), 
-    28080003   :	( 80,  'MAJADAHONDA'), 
-    28092005   :	( 92,  'MÓSTOLES'), 
-    28102001   :	(102,  'ORUSCO DE TAJUÑA'), 
-    28120001   : 	(120,  'PUERTO DE COTOS'), 
-    28123002   :	(123,  'RIVAS-VACIAMADRID'), 
-    28133002   :	(133,  'SAN MARTÍN DE VALDEIGLESIAS'), 
-    28148004   :	(148,  'TORREJÓN DE ARDOZ'), 
-    28161001   :	(161,  'VALDEMORO'), 
-    28171001   :	(171,  'VILLA DEL PRADO'), 
-    28180001   :	(180,  'VILLAREJO DE SALVANÉS')
-    }
-    
-    # Listado de magnitudes
-    # ---------------------
-    maglist = {
-    # código     magnitud                unidades    
-    81 :	('Velocidad del viento',     'm/s'), 
-    82 :	('Dirección del viento',     'º'), 
-    83 :	('Temperatura',              'ºC'), 
-    86 :	('Humedad relativa',         '%'), 
-    87 :	('Presión atmosférica',      'hPa'), 
-    88 :	('Radiación solar',          'W/m2'), 
-    89 :	('Precipitación',            'mm')
-    }
-
 
     # Lectura de datos
     # -------------------------------------------
@@ -338,7 +331,7 @@ def meteo(ifile, codigo_magnitud=83, codigo_estacion=28067001) :
     # --------------------------------------
     df = df[ (df['estacion']  == int(str(codigo_estacion)[-3:])) 
            & (df['magnitud']  == codigo_magnitud) 
-           & (df['municipio'] == estalist[codigo_estacion][0])]
+           & (df['municipio'] == estalist_meteo[codigo_estacion][0])]
     
     # Tamaño de la muestra
     # -------------------------
@@ -357,13 +350,12 @@ def meteo(ifile, codigo_magnitud=83, codigo_estacion=28067001) :
                  var_name='hora',
                  value_name='valor',  
                  )
-    
-    
+        
     # Convertimos la hora a valor numérico
     # --------------------------------------------------
     df1['hora'] = df1['hora'].apply(lambda x : int(x[1:]))
     
- 
+
     # Pasamos los días de columnas a filas con
     # el flag de calidad como valor
     # -----------------------------------------
@@ -405,7 +397,7 @@ def meteo(ifile, codigo_magnitud=83, codigo_estacion=28067001) :
     # ------------------------------------------
     df.sort_index(inplace=True)
     
-    return df, *maglist[codigo_magnitud], estalist[codigo_estacion][1]
+    return df, *maglist_meteo[codigo_magnitud], estalist_meteo[codigo_estacion][1]
 
 
 
